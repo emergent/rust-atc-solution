d5f5dc5894b60cfdab017d377317be111aee66ac
Satoshi Yoshikawa <yosikawa@gmail.com>
2019-06-19 08:01:44 +0900

    solved abc124a&b in Rust

---
+++ src/bin/abc124a.rs
@@ -0,0 +1,31 @@
+fn read<T: std::str::FromStr>() -> T {
+    let mut s = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    s.trim().parse().ok().unwrap()
+}
+
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read::<String>()
+        .split_whitespace()
+        .map(|e| e.parse().ok().unwrap())
+        .collect()
+}
+
+#[allow(dead_code)]
+fn read_vec2<T: std::str::FromStr>(n: u32) -> Vec<Vec<T>> {
+    (0..n).map(|_| read_vec()).collect()
+}
+
+fn main() {
+    let v = read_vec::<u32>();
+    let a = v[0];
+    let b = v[1];
+
+    let bigger = std::cmp::max(a, b);
+    let ans = if a != b {
+        bigger * 2 - 1
+    } else {
+        bigger * 2
+    };
+    println!("{}", ans);
+}
