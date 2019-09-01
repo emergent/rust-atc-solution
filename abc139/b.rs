5ec337c9b70c0888b385120a9f0316c727cf2b06
emergent <yosikawa@gmail.com>
2019-09-02 01:04:38 +0900

    solved abc139 a,b,c,d in Rust

---
+++ abc139/b.rs
@@ -0,0 +1,43 @@
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    let mut s = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    s.trim().parse().ok().unwrap()
+}
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read::<String>()
+        .split_whitespace()
+        .map(|e| e.parse().ok().unwrap())
+        .collect()
+}
+#[allow(dead_code)]
+fn read_vec2<T: std::str::FromStr>(n: u32) -> Vec<Vec<T>> {
+    (0..n).map(|_| read_vec()).collect()
+}
+#[allow(dead_code)]
+fn yn(result: bool) {
+    if result {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
+}
+
+fn main() {
+    let v = read_vec::<u32>();
+    let a = v[0];
+    let b = v[1];
+
+    let mut count = 0;
+    let mut kuchi = 1;
+    loop {
+        if kuchi >= b {
+            break;
+        }
+
+        kuchi += a - 1;
+        count += 1;
+    }
+    println!("{}", count);
+}
