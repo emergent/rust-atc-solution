5c7ecd9957f5b7b3c444621521564f4503ff677a
emergent <yosikawa@gmail.com>
2019-12-01 20:39:37 +0900

    update template

--- template/a.rs
+++ template/a.rs
@@ -1,29 +1,3 @@
-#[allow(dead_code)]
-fn read<T: std::str::FromStr>() -> T {
-    let mut s = String::new();
-    std::io::stdin().read_line(&mut s).ok();
-    s.trim().parse().ok().unwrap()
-}
-#[allow(dead_code)]
-fn read_vec<T: std::str::FromStr>() -> Vec<T> {
-    read::<String>()
-        .split_whitespace()
-        .map(|e| e.parse().ok().unwrap())
-        .collect()
-}
-#[allow(dead_code)]
-fn read_vec2<T: std::str::FromStr>(n: u32) -> Vec<Vec<T>> {
-    (0..n).map(|_| read_vec()).collect()
-}
-#[allow(dead_code)]
-fn yn(result: bool) {
-    if result {
-        println!("Yes");
-    } else {
-        println!("No");
-    }
-}
-
 fn main() {
     /* single input
     let n = read::<usize>();
@@ -50,4 +24,30 @@ fn main() {
     /* yes/no pattern
     yn(a == b);
     */
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    let mut s = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    s.trim().parse().ok().unwrap()
+}
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read::<String>()
+        .split_whitespace()
+        .map(|e| e.parse().ok().unwrap())
+        .collect()
+}
+#[allow(dead_code)]
+fn read_vec2<T: std::str::FromStr>(n: u32) -> Vec<Vec<T>> {
+    (0..n).map(|_| read_vec()).collect()
+}
+#[allow(dead_code)]
+fn yn(result: bool) {
+    if result {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
 }
