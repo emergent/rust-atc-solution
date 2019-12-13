8772426cc747cd921218724447230dec456a1a14
emergent <yosikawa@gmail.com>
2019-12-13 23:39:28 +0900

    solved problem A,B of ABC074 in Rust

---
+++ abc074/b.rs
@@ -0,0 +1,39 @@
+fn main() {
+    let n = read::<usize>();
+    let k = read::<i32>();
+    let xs = read_vec::<i32>();
+
+    let mut sum = 0;
+    for i in 0..n {
+        let p = std::cmp::min(xs[i], k - xs[i]);
+
+        sum += p * 2;
+    }
+    println!("{}", sum);
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    let mut s = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    s.trim().parse().ok().unwrap()
+}
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read::<String>()
+        .split_whitespace()
+        .map(|e| e.parse().ok().unwrap())
+        .collect()
+}
+#[allow(dead_code)]
+fn read_vec2<T: std::str::FromStr>(n: u32) -> Vec<Vec<T>> {
+    (0..n).map(|_| read_vec()).collect()
+}
+#[allow(dead_code)]
+fn yn(result: bool) {
+    if result {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
+}
