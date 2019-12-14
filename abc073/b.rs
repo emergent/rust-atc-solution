aa0346c8455ff533a31327f1e56afda59decc660
emergent <yosikawa@gmail.com>
2019-12-14 13:53:29 +0900

    solved problem A,B of ABC073 in Rust

---
+++ abc073/b.rs
@@ -0,0 +1,38 @@
+fn main() {
+    let n = read::<usize>();
+
+    let mut sum = 0;
+    for _ in 0..n {
+        let v = read_vec::<u32>();
+        let l = v[0];
+        let r = v[1];
+        sum += r - l + 1;
+    }
+    println!("{}", sum);
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    let mut s = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    s.trim().parse().ok().unwrap()
+}
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read::<String>()
+        .split_whitespace()
+        .map(|e| e.parse().ok().unwrap())
+        .collect()
+}
+#[allow(dead_code)]
+fn read_vec2<T: std::str::FromStr>(n: u32) -> Vec<Vec<T>> {
+    (0..n).map(|_| read_vec()).collect()
+}
+#[allow(dead_code)]
+fn yn(result: bool) {
+    if result {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
+}
