0f0cea4f7d81ee9438dd208c6ca77527af3405a0
emergent <yosikawa@gmail.com>
2019-12-14 14:49:11 +0900

    solved problem A,B of ABC067 in Rust

---
+++ abc067/b.rs
@@ -0,0 +1,38 @@
+fn main() {
+    let v = read_vec::<usize>();
+    let _n = v[0];
+    let k = v[1];
+    let mut ls = read_vec::<i32>();
+    ls.sort_by_key(|l| -l);
+    let mut sum = 0;
+    for i in 0..k {
+        sum += ls[i];
+    }
+    println!("{}", sum);
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    let mut s = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    s.trim().parse().ok().unwrap()
+}
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read::<String>()
+        .split_whitespace()
+        .map(|e| e.parse().ok().unwrap())
+        .collect()
+}
+#[allow(dead_code)]
+fn read_vec2<T: std::str::FromStr>(n: u32) -> Vec<Vec<T>> {
+    (0..n).map(|_| read_vec()).collect()
+}
+#[allow(dead_code)]
+fn yn(result: bool) {
+    if result {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
+}
