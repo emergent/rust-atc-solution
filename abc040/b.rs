eab5ddfebfc818a608a8efabcf27fcb4340dcb56
emergent <yosikawa@gmail.com>
2019-12-15 18:16:02 +0900

    solved problem A,B of ABC040 in Rust

---
+++ abc040/b.rs
@@ -0,0 +1,35 @@
+fn main() {
+    let n = read::<i64>();
+    let mut min = 100000;
+    for i in 1..n + 1 {
+        let x = (i - n / i).abs() + n - (i * (n / i));
+        min = std::cmp::min(x, min);
+    }
+    println!("{}", min);
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    let mut s = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    s.trim().parse().ok().unwrap()
+}
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read::<String>()
+        .split_whitespace()
+        .map(|e| e.parse().ok().unwrap())
+        .collect()
+}
+#[allow(dead_code)]
+fn read_vec2<T: std::str::FromStr>(n: u32) -> Vec<Vec<T>> {
+    (0..n).map(|_| read_vec()).collect()
+}
+#[allow(dead_code)]
+fn yn(result: bool) {
+    if result {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
+}
