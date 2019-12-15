8a7ed213eed7ac24ad9e9ee529300b7a29d3abb5
emergent <yosikawa@gmail.com>
2019-12-15 14:11:00 +0900

    solved problem A,B of ABC056 in Rust & Python

---
+++ abc056/b.rs
@@ -0,0 +1,33 @@
+fn main() {
+    let v = read_vec::<i32>();
+    let w = v[0];
+    let a = v[1];
+    let b = v[2];
+    println!("{}", std::cmp::max((a - b).abs() - w, 0));
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    let mut s = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    s.trim().parse().ok().unwrap()
+}
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read::<String>()
+        .split_whitespace()
+        .map(|e| e.parse().ok().unwrap())
+        .collect()
+}
+#[allow(dead_code)]
+fn read_vec2<T: std::str::FromStr>(n: u32) -> Vec<Vec<T>> {
+    (0..n).map(|_| read_vec()).collect()
+}
+#[allow(dead_code)]
+fn yn(result: bool) {
+    if result {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
+}
