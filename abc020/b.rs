6dc6e8c9333064717af2abfbcea9fa1a78db8f0d
emergent <yosikawa@gmail.com>
2019-12-21 16:37:43 +0900

    solved problem A,B of ABC020 in Rust

---
+++ abc020/b.rs
@@ -0,0 +1,31 @@
+fn main() {
+    let s = read_vec::<String>();
+    let s2 = s.join("");
+    println!("{}", s2.parse::<u32>().unwrap() * 2);
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    let mut s = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    s.trim().parse().ok().unwrap()
+}
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read::<String>()
+        .split_whitespace()
+        .map(|e| e.parse().ok().unwrap())
+        .collect()
+}
+#[allow(dead_code)]
+fn read_vec2<T: std::str::FromStr>(n: u32) -> Vec<Vec<T>> {
+    (0..n).map(|_| read_vec()).collect()
+}
+#[allow(dead_code)]
+fn yn(result: bool) {
+    if result {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
+}
