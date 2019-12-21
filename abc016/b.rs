3425514da03a7784b98c6049c1adab9c822bab69
emergent <yosikawa@gmail.com>
2019-12-21 17:25:22 +0900

    solved problem A,B of ABC016 in Rust

---
+++ abc016/b.rs
@@ -0,0 +1,42 @@
+fn main() {
+    let v = read_vec::<u32>();
+    let a = v[0];
+    let b = v[1];
+    let c = v[2];
+
+    if b == 0 && a == c {
+        println!("?");
+    } else if a + b == c {
+        println!("+");
+    } else if a - b == c {
+        println!("-");
+    } else {
+        println!("!");
+    }
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    let mut s = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    s.trim().parse().ok().unwrap()
+}
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read::<String>()
+        .split_whitespace()
+        .map(|e| e.parse().ok().unwrap())
+        .collect()
+}
+#[allow(dead_code)]
+fn read_vec2<T: std::str::FromStr>(n: u32) -> Vec<Vec<T>> {
+    (0..n).map(|_| read_vec()).collect()
+}
+#[allow(dead_code)]
+fn yn(result: bool) {
+    if result {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
+}
