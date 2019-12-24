2b5d4df01872f17a2fd2ba6a19dbd00f94bfeaa2
emergent <yosikawa@gmail.com>
2019-12-24 12:46:12 +0900

    solved problem A,B of ABC010 in Rust

---
+++ abc010/b.rs
@@ -0,0 +1,39 @@
+fn main() {
+    let n = read::<usize>();
+    let mut a = read_vec::<u32>();
+    let mut count = 0;
+    for i in 0..n {
+        while a[i] % 2 == 0 || a[i] % 6 == 5 {
+            //println!("{}, {}", i, a[i]);
+            a[i] -= 1;
+            count += 1;
+        }
+    }
+    println!("{}", count);
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    let mut s = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    s.trim().parse().ok().unwrap()
+}
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read::<String>()
+        .split_whitespace()
+        .map(|e| e.parse().ok().unwrap())
+        .collect()
+}
+#[allow(dead_code)]
+fn read_vec2<T: std::str::FromStr>(n: u32) -> Vec<Vec<T>> {
+    (0..n).map(|_| read_vec()).collect()
+}
+#[allow(dead_code)]
+fn yn(result: bool) {
+    if result {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
+}
