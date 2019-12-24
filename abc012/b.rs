269f5453bd247694b074d9c38c6968e147032777
emergent <yosikawa@gmail.com>
2019-12-24 11:41:42 +0900

    solved problem A,B of ABC012 in Rust

---
+++ abc012/b.rs
@@ -0,0 +1,34 @@
+fn main() {
+    let mut n = read::<u32>();
+    let h = n / 3600;
+    n %= 3600;
+    let m = n / 60;
+    n %= 60;
+    println!("{0:>02}:{1:>02}:{2:>02}", h, m, n);
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    let mut s = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    s.trim().parse().ok().unwrap()
+}
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read::<String>()
+        .split_whitespace()
+        .map(|e| e.parse().ok().unwrap())
+        .collect()
+}
+#[allow(dead_code)]
+fn read_vec2<T: std::str::FromStr>(n: u32) -> Vec<Vec<T>> {
+    (0..n).map(|_| read_vec()).collect()
+}
+#[allow(dead_code)]
+fn yn(result: bool) {
+    if result {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
+}
