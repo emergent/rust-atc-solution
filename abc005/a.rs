4cb761886f6e905aa0a7dd3f56e437153672fec6
emergent <yosikawa@gmail.com>
2019-12-25 18:31:52 +0900

    solved problem A,B of ABC005 in Rust

---
+++ abc005/a.rs
@@ -0,0 +1,32 @@
+fn main() {
+    let v = read_vec::<u32>();
+    let a = v[0];
+    let b = v[1];
+    println!("{}", b / a);
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    let mut s = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    s.trim().parse().ok().unwrap()
+}
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read::<String>()
+        .split_whitespace()
+        .map(|e| e.parse().ok().unwrap())
+        .collect()
+}
+#[allow(dead_code)]
+fn read_vec2<T: std::str::FromStr>(n: u32) -> Vec<Vec<T>> {
+    (0..n).map(|_| read_vec()).collect()
+}
+#[allow(dead_code)]
+fn yn(result: bool) {
+    if result {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
+}
