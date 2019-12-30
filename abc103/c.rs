840d3b1aa495c10064df720a05d9acce92085cf1
emergent <yosikawa@gmail.com>
2019-12-30 12:10:56 +0900

    solved problem C of ABC103 in Rust

---
+++ abc103/c.rs
@@ -0,0 +1,32 @@
+fn main() {
+    let _ = read::<usize>();
+    let a = read_vec::<u64>();
+    let ans = a.into_iter().map(|x| x-1).sum::<u64>();
+    println!("{}", ans);
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    let mut s = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    s.trim().parse().ok().unwrap()
+}
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read::<String>()
+        .split_whitespace()
+        .map(|e| e.parse().ok().unwrap())
+        .collect()
+}
+#[allow(dead_code)]
+fn read_vec2<T: std::str::FromStr>(n: u32) -> Vec<Vec<T>> {
+    (0..n).map(|_| read_vec()).collect()
+}
+#[allow(dead_code)]
+fn yn(result: bool) {
+    if result {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
+}
