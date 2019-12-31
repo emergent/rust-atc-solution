717eebbfc028033423e27377b60c6a9b664103a5
emergent <yosikawa@gmail.com>
2019-12-31 14:35:31 +0900

    solved problem C of ABC093 in Rust

---
+++ abc093/c.rs
@@ -0,0 +1,36 @@
+fn main() {
+    let mut v = read_vec::<u32>();
+    v.sort();
+    let diff = (v[2] - v[0]) + (v[2] - v[1]);
+    if diff % 2 == 0 {
+        println!("{}", diff / 2);
+    } else {
+        println!("{}", diff / 2 + 2);
+    }
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    let mut s = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    s.trim().parse().ok().unwrap()
+}
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read::<String>()
+        .split_whitespace()
+        .map(|e| e.parse().ok().unwrap())
+        .collect()
+}
+#[allow(dead_code)]
+fn read_vec2<T: std::str::FromStr>(n: u32) -> Vec<Vec<T>> {
+    (0..n).map(|_| read_vec()).collect()
+}
+#[allow(dead_code)]
+fn yn(result: bool) {
+    if result {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
+}
