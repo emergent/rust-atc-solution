b56d7e679ad0f212566a77ea6cd8e083f1207e14
emergent <yosikawa@gmail.com>
2020-01-05 02:32:22 +0900

    solved problem C of ABC101 in Rust

---
+++ abc101/c.rs
@@ -0,0 +1,37 @@
+fn main() {
+    let v = read_vec::<usize>();
+    let n = v[0];
+    let k = v[1];
+    let _ = read_vec::<u32>();
+    if n == k {
+        println!("{}", 1);
+    } else {
+        println!("{}", ((n - 1) + (k - 1) - 1) / (k - 1));
+    }
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    let mut s = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    s.trim().parse().ok().unwrap()
+}
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read::<String>()
+        .split_whitespace()
+        .map(|e| e.parse().ok().unwrap())
+        .collect()
+}
+#[allow(dead_code)]
+fn read_vec2<T: std::str::FromStr>(n: u32) -> Vec<Vec<T>> {
+    (0..n).map(|_| read_vec()).collect()
+}
+#[allow(dead_code)]
+fn yn(result: bool) {
+    if result {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
+}
