dd286f189e1026b0153b524a6c242a38682c9385
emergent <yosikawa@gmail.com>
2020-01-06 01:22:49 +0900

    solved problem C of ABC056 in Rust

---
+++ abc056/c.rs
@@ -0,0 +1,35 @@
+fn main() {
+    let mut x = read::<i32>();
+    let mut i = 0;
+    while x > 0 {
+        i += 1;
+        x -= i;
+    }
+    println!("{}", i);
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    let mut s = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    s.trim().parse().ok().unwrap()
+}
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read::<String>()
+        .split_whitespace()
+        .map(|e| e.parse().ok().unwrap())
+        .collect()
+}
+#[allow(dead_code)]
+fn read_vec2<T: std::str::FromStr>(n: u32) -> Vec<Vec<T>> {
+    (0..n).map(|_| read_vec()).collect()
+}
+#[allow(dead_code)]
+fn yn(result: bool) {
+    if result {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
+}
