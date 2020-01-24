a6b57ad702718b6f63225ccad76514c60abc2ed2
emergent <yosikawa@gmail.com>
2020-01-24 14:19:26 +0900

    solved problem C of ABC055 in Rust

---
+++ abc055/c.rs
@@ -0,0 +1,37 @@
+use std::cmp::*;
+fn main() {
+    let v = read_vec::<u64>();
+    let n = v[0];
+    let m = v[1];
+    let mut count = 0;
+    count += min(n, m / 2);
+    let rest_c = m - count * 2;
+    count += rest_c / 4;
+    println!("{}", count);
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    let mut s = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    s.trim().parse().ok().unwrap()
+}
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read::<String>()
+        .split_whitespace()
+        .map(|e| e.parse().ok().unwrap())
+        .collect()
+}
+#[allow(dead_code)]
+fn read_vec2<T: std::str::FromStr>(n: u32) -> Vec<Vec<T>> {
+    (0..n).map(|_| read_vec()).collect()
+}
+#[allow(dead_code)]
+fn yn(result: bool) {
+    if result {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
+}
