5021a89deebbf4774361c6158a644691473d4c0c
emergent <yosikawa@gmail.com>
2020-02-02 17:32:44 +0900

    solved problem C of ABC053 in Rust

---
+++ abc053/c.rs
@@ -0,0 +1,37 @@
+fn main() {
+    let mut n = read::<u64>();
+    let count = (n / 11) * 2;
+    n %= 11;
+    let ans = match n {
+        0 => count,
+        1...6 => count + 1,
+        _ => count + 2,
+    };
+    println!("{}", ans);
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    let mut s = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    s.trim().parse().ok().unwrap()
+}
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read::<String>()
+        .split_whitespace()
+        .map(|e| e.parse().ok().unwrap())
+        .collect()
+}
+#[allow(dead_code)]
+fn read_vec2<T: std::str::FromStr>(n: u32) -> Vec<Vec<T>> {
+    (0..n).map(|_| read_vec()).collect()
+}
+#[allow(dead_code)]
+fn yn(result: bool) {
+    if result {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
+}
