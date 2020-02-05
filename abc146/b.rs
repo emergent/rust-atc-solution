bb1ce9ef3d8247168fa1a48acb559da1959f0496
emergent <yosikawa@gmail.com>
2020-02-05 13:51:28 +0900

    solved problem B of ABC146 in Rust

---
+++ abc146/b.rs
@@ -0,0 +1,34 @@
+fn main() {
+    let n = read::<u8>();
+    let s = read::<String>()
+        .chars()
+        .map(|c| (((c as u8 - 65 + n) % 26) + 65) as char)
+        .collect::<String>();
+    println!("{}", s);
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    let mut s = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    s.trim().parse().ok().unwrap()
+}
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read::<String>()
+        .split_whitespace()
+        .map(|e| e.parse().ok().unwrap())
+        .collect()
+}
+#[allow(dead_code)]
+fn read_vec2<T: std::str::FromStr>(n: u32) -> Vec<Vec<T>> {
+    (0..n).map(|_| read_vec()).collect()
+}
+#[allow(dead_code)]
+fn yn(result: bool) {
+    if result {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
+}
