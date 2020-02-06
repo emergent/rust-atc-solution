13a02cecddc284e2f1615fab043f041e1382125f
emergent <yosikawa@gmail.com>
2020-02-06 22:48:58 +0900

    solved problem A of ABC149 in Rust

---
+++ abc149/a.rs
@@ -0,0 +1,33 @@
+fn main() {
+    let v = read_vec::<String>();
+    let mut ans = "".to_string();
+    ans.push_str(&v[1]);
+    ans.push_str(&v[0]);
+    println!("{}", ans);
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    let mut s = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    s.trim().parse().ok().unwrap()
+}
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read::<String>()
+        .split_whitespace()
+        .map(|e| e.parse().ok().unwrap())
+        .collect()
+}
+#[allow(dead_code)]
+fn read_vec2<T: std::str::FromStr>(n: u32) -> Vec<Vec<T>> {
+    (0..n).map(|_| read_vec()).collect()
+}
+#[allow(dead_code)]
+fn yn(result: bool) {
+    if result {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
+}
