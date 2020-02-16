423494c21fbb284786563dde599f010bf3418d21
emergent <yosikawa@gmail.com>
2020-02-16 23:10:54 +0900

    solved problem A,B,C of ABC155 in Rust

---
+++ abc155/b.rs
@@ -0,0 +1,41 @@
+fn main() {
+    let _n = read::<usize>();
+    let v = read_vec::<u32>();
+    for i in v {
+        if i % 2 == 0 {
+            if i % 3 == 0 || i % 5 == 0 {
+                continue;
+            } else {
+                println!("DENIED");
+                return;
+            }
+        }
+    }
+    println!("APPROVED");
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    let mut s = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    s.trim().parse().ok().unwrap()
+}
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read::<String>()
+        .split_whitespace()
+        .map(|e| e.parse().ok().unwrap())
+        .collect()
+}
+#[allow(dead_code)]
+fn read_vec2<T: std::str::FromStr>(n: u32) -> Vec<Vec<T>> {
+    (0..n).map(|_| read_vec()).collect()
+}
+#[allow(dead_code)]
+fn yn(result: bool) {
+    if result {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
+}
